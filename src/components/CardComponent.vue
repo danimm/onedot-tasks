03275efde7f2a841325235c37a4e0485b89b0411
Daniel Muñoz <info@gmail.com>
2020-01-31 11:37:20 +0100

    card component with data created

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img :src="phone.image" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body :title="phone.product">
+          <b-card-text>
+            This is a wider card with supporting text as a natural lead-in to additional
+            content. This content is a little bit longer.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    phone: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(phone, index) in phones" :key="index" class="grid-container">
+      <CardComponent :phone="phone" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CardComponent from "@/components/CardComponent.vue";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: { CardComponent },
+  data() {
+    return {
+      phones: [
+        {
+          product: "Apple Iphone 6s",
+          color: "Dark Grey",
+          price: "CHF 769",
+          image:
+            "https://static.digitecgalaxus.ch/Files/4/8/9/3/1/2/1/iPhone6s_2up_PB_PF_SpGry_CH-DE-SCREEN.jpg?fit=inside%7C1823:2000"
+        },
+        {
+          product: "Samsung Galaxy S8",
+          color: "Midnight Black",
+          price: "CHF 569",
+          image:
+            "https://static.digitecgalaxus.ch/Files/1/9/8/1/1/5/9/4/Samsung_S10_black.jpg?fit=inside%7C1823:2000"
+        },
+        {
+          product: "Huawei P9",
+          color: "Mystic Silver",
+          price: "CHF 272",
+          image:
+            "https://static.digitecgalaxus.ch/Files/7/4/7/4/3/6/8/0006499_huawei-p9-eva-l09-32gb-titanium-grey.jpeg?fit=inside%7C1823:2000"
+        }
+      ]
+    };
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  padding: 40px;
+  margin: 0 auto;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 2fr;
+}
+</style>
