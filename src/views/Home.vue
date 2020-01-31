5cc6bce884f81d96fee53de52d4b90851fb2f40a
Daniel Muñoz <info@gmail.com>
2020-01-31 13:11:38 +0100

    move the hardcore data to the store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Welcome to the ABC Company</h1>
+    <h2>The best e-commerce in the world</h2>
   </div>
 </template>
 
@@ -13,4 +14,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  height: 80vh;
+  background-image: url("https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80");
+}
+h1 {
+  color: white;
+}
+</style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -7,37 +7,19 @@
 </template>
 
 <script>
+//components
 import CardComponent from "@/components/CardComponent";
+
+import { mapState } from "vuex";
 
 export default {
   name: "overview",
   components: { CardComponent },
   data() {
-    return {
-      phones: [
-        {
-          product: "Apple Iphone 6s",
-          color: "Dark Grey",
-          price: "CHF 769",
-          image:
-            "https://static.digitecgalaxus.ch/Files/4/8/9/3/1/2/1/iPhone6s_2up_PB_PF_SpGry_CH-DE-SCREEN.jpg?fit=inside%7C1823:2000"
-        },
-        {
-          product: "Samsung Galaxy S8",
-          color: "Midnight Black",
-          price: "CHF 569",
-          image:
-            "https://static.digitecgalaxus.ch/Files/1/9/8/1/1/5/9/4/Samsung_S10_black.jpg?fit=inside%7C1823:2000"
-        },
-        {
-          product: "Huawei P9",
-          color: "Mystic Silver",
-          price: "CHF 272",
-          image:
-            "https://static.digitecgalaxus.ch/Files/7/4/7/4/3/6/8/0006499_huawei-p9-eva-l09-32gb-titanium-grey.jpeg?fit=inside%7C1823:2000"
-        }
-      ]
-    };
+    return {};
+  },
+  computed: {
+    ...mapState(["phones"])
   }
 };
 </script>
